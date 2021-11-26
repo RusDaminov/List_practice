38cd2a81d5b37ca12c9c7947d8db64349839052b
Arshavin Bernita <abernita@ge-g6.kzn.21-school.ru>
2021-11-26 03:31:00 +0300

    3

--- main.c
+++ main.c
@@ -11,15 +11,24 @@ struct Node {
 int main() {
 
 	Node * list = NULL;
-	Node a = {3}, b = {17}, c = {21}, t = {10};
+	Node a = {3};
+	Node b = {17};
+	Node c = {21};
+	Node t = {10};
+
 	list = &a;
 	a.next = &b;
 	b.next = &c;
 	c.next = NULL;
 
-	printf("%d ", a.data);
-	printf("%d ", b.data);
-	printf("%d ", c.data);
+	Node * p;
+	p = list;
+
+	printf("%d ", p->data);
+	p = p->next;
+	printf("%d ", p->data);
+	p = p->next;
+	printf("%d ", p->data);
 	printf("\n");
 
 	return 0;
